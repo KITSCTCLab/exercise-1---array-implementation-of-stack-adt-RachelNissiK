121c860ab29b69e42dd61e11cd503e6e035889ca
URK21CS3016 <109951881+URK21CS3016@users.noreply.github.com>
2022-07-25 15:15:43 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,42 +1,41 @@
 import os
- def __init__(self,n):
-        self.n=n
-        self.st=[None]*n
-        self.top=-1
-    def isFull(self) :
-        return self.top==n-1
-    def isEmpty(self):
-        return self.top==n+1
-    def push(self):
-        if self.isFull():
-            print("the stack is full")
-        else:
+class Stack:
+    def __init__(self, size):
+        self.items = []
+        self.size = size
+
+    def is_empty(self):
+        return self.top==-1
+
+    def is_full(self):
+        return self.top==(self.maxsize-1)
+
+    def push(self, data):
+        if not self.is_full():
             self.top+=1
-            x=int(input("enter the data to be inserted"))
-            self.st[self.top]=x
+            self.stack[self.top]=data
+        else:
+            print("Stack is full")
+
     def pop(self):
-        if self.isEmpty():
-             self.top-=1       
-        else:    
-            self.st.pop()
-    def display(self):
-        for i in range(self.top+1):
-              final=self.st
-        print(final)        
-    def peek(self):
-        print(self.st[-1])
-n =int(input("enter the size of stack"))
-s1=stack1(n)
-while(1):
-    print("1.push\n2.pop\n3.display\n4.peek\n5.exit")
-    ch=int(input("enter the choice"))
-    if ch==1:
-        s1.push()
-    elif ch==2:
-        s1.pop()
-    elif ch==3:
-        s1.display()
-    elif ch==4:
-        s1.peek()
-    else:
-         break
+        if not self.is_empty():
+            x=self.stack[self.top]
+            self.top-=1
+            return x
+        else:
+            print("Stack is empty")
+
+    def status(self):
+        # Write code here
+
+# Do not change the following code
+size, queries = map(int, input().rstrip().split())
+stack = Stack(size)
+for line in range(queries):
+    values = list(map(int, input().rstrip().split()))
+    if values[0] == 1:
+        stack.push(values[1])
+    elif values[0] == 2:
+        stack.pop()
+stack.status()
+
