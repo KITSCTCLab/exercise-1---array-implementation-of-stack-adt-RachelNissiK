b983f992e3dc4bb954e4e2ae75dc02e7af327ae9
URK21CS3016 <109951881+URK21CS3016@users.noreply.github.com>
2022-07-25 15:03:24 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,33 +1,42 @@
 import os
-class Stack:
-    def __init__(self, size):
-        self.items = []
-        self.size = size
-
-    def is_empty(self):
-        # Write code here
-
-    def is_full(self):
-        # Write code here
-
-    def push(self, data):
-        if not self.is_full():
-            # Write code here
-
+ def __init__(self,n):
+        self.n=n
+        self.st=[None]*n
+        self.top=-1
+    def isFull(self) :
+        return self.top==n-1
+    def isEmpty(self):
+        return self.top==n+1
+    def push(self):
+        if self.isFull():
+            print("the stack is full")
+        else:
+            self.top+=1
+            x=int(input("enter the data to be inserted"))
+            self.st[self.top]=x
     def pop(self):
-        if not self.is_empty():
-            # Write code here
-
-    def status(self):
-        # Write code here
-
-# Do not change the following code
-size, queries = map(int, input().rstrip().split())
-stack = Stack(size)
-for line in range(queries):
-    values = list(map(int, input().rstrip().split()))
-    if values[0] == 1:
-        stack.push(values[1])
-    elif values[0] == 2:
-        stack.pop()
-stack.status()
+        if self.isEmpty():
+             self.top-=1       
+        else:    
+            self.st.pop()
+    def display(self):
+        for i in range(self.top+1):
+              final=self.st
+        print(final)        
+    def peek(self):
+        print(self.st[-1])
+n =int(input("enter the size of stack"))
+s1=stack1(n)
+while(1):
+    print("1.push\n2.pop\n3.display\n4.peek\n5.exit")
+    ch=int(input("enter the choice"))
+    if ch==1:
+        s1.push()
+    elif ch==2:
+        s1.pop()
+    elif ch==3:
+        s1.display()
+    elif ch==4:
+        s1.peek()
+    else:
+         break
